c976e3274d9853eefd5d8c9316c5821b260fe129
daAnis <daria.anisimowa@yandex.ru>
2020-12-13 00:30:18 +0400

    login+signup

--- Tetris2/app/src/main/java/ru/ssau/fiit/tetris/MainActivity.java
+++ Tetris2/app/src/main/java/ru/ssau/fiit/tetris/MainActivity.java
@@ -2,13 +2,34 @@ package ru.ssau.fiit.tetris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox login;
+    private CheckBox sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (CheckBox) findViewById(R.id.login);
+        sign_up = (CheckBox) findViewById(R.id.sign_up);
+    }
+
+    public void loginClick(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void signUpClick(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
