e76b86c143bda20cebacda1aab603731d8fb0339
daAnis <daria.anisimowa@yandex.ru>
2021-01-07 23:28:01 +0400

    glass activity

--- Tetris_3/android/src/ru/ssau/fiit/tetris/GlassActivity.java
+++ Tetris_3/android/src/ru/ssau/fiit/tetris/GlassActivity.java
@@ -12,11 +12,12 @@ import top.defaults.colorpicker.ColorObserver;
 import top.defaults.colorpicker.ColorPickerView;
 
 public class GlassActivity extends AppCompatActivity {
-    GlassView glassView;
-    EditText width_editable, height_editable;
-    ColorPickerView colorPickerView;
-    Glass glass;
-    int width, height;
+    private GlassView glassView;
+    private EditText width_editable, height_editable, speed_editable, points_editable;
+    private ColorPickerView colorPickerView;
+    private Glass glass;
+    private int width, height;
+    private double speed, points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,59 +28,16 @@ public class GlassActivity extends AppCompatActivity {
         Bundle arguments = getIntent().getExtras();
         if (arguments != null)
             glass = (Glass) arguments.getSerializable(Glass.class.getSimpleName());
+
         init();
-
-        /*
-        //перерисовать стакан при изменении размеров
-        glassView = findViewById(R.id.new_glass);
-        width_editable = findViewById(R.id.w);
-        height_editable = findViewById(R.id.h);
-        Button drawGlass = findViewById(R.id.draw_glass);
-        drawGlass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    width = Integer.parseInt(width_editable.getText().toString());
-                } catch (NumberFormatException e) {
-                    Toast.makeText(GlassActivity.this, "Ширина задана неверно!", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                try {
-                    height = Integer.parseInt(height_editable.getText().toString());
-                } catch (NumberFormatException e) {
-                    Toast.makeText(GlassActivity.this, "Высота задана неверно!", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                glass.setWidth(width);
-                glass.setHeight(height);
-                glassView.setGlass(glass);
-            }
-        });
-
-        //изменение цвета
-        colorPickerView = findViewById(R.id.color_picker);
-        colorPickerView.subscribe(new ColorObserver() {
-            @Override
-            public void onColor(int color, boolean fromUser, boolean shouldPropagate) {
-                glassView.setGlassColor(color);
-            }
-        });
-        colorPickerView.setInitialColor(getResources().getColor(R.color.check_box_color));
-
-        //сохранение изменений
-        Button saveGlass = findViewById(R.id.save_glass);
-        saveGlass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });*/
     }
 
     private void init() {
         glassView = findViewById(R.id.new_glass);
         width_editable = findViewById(R.id.w);
         height_editable = findViewById(R.id.h);
+        speed_editable = findViewById(R.id.speed_k);
+        points_editable = findViewById(R.id.points_k);
 
         //изменение цвета
         colorPickerView = findViewById(R.id.color_picker);
@@ -87,6 +45,7 @@ public class GlassActivity extends AppCompatActivity {
             @Override
             public void onColor(int color, boolean fromUser, boolean shouldPropagate) {
                 glassView.setGlassColor(color);
+                glass.setColor(color);
             }
         });
 
@@ -95,7 +54,8 @@ public class GlassActivity extends AppCompatActivity {
             width_editable.setText(glass.getWidth());
             height_editable.setText(glass.getHeight());
             colorPickerView.setInitialColor(glass.getColor());
-
+            speed_editable.setText(String.format("%s", glass.getSpeed_k()));
+            points_editable.setText(String.format("%s", glass.getPoints_k()));
             glassView.setGlass(glass);
         } else {
             glass = new Glass();
@@ -113,12 +73,14 @@ public class GlassActivity extends AppCompatActivity {
                     Toast.makeText(GlassActivity.this, "Ширина задана неверно!", Toast.LENGTH_LONG).show();
                     return;
                 }
+                glass.setWidth(width);
                 try {
                     height = Integer.parseInt(height_editable.getText().toString());
                 } catch (NumberFormatException e) {
                     Toast.makeText(GlassActivity.this, "Высота задана неверно!", Toast.LENGTH_LONG).show();
                     return;
                 }
+                glass.setHeight(height);
                 glassView.setGlassSize(width, height);
             }
         });
@@ -128,8 +90,33 @@ public class GlassActivity extends AppCompatActivity {
         saveGlass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //glassView.getGlass().setPoints_k();
+                try {
+                    speed = Double.parseDouble(speed_editable.getText().toString());
+                } catch (NumberFormatException e) {
+                    Toast.makeText(GlassActivity.this, "Скорость задана неверно!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                glass.setSpeed_k(speed);
+                try {
+                    points = Double.parseDouble(points_editable.getText().toString());
+                } catch (NumberFormatException e) {
+                    Toast.makeText(GlassActivity.this, "Количество очков задано неверно!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                glass.setPoints_k(points);
+                //todo проверка на уникальность
+                //todo передача стакана в бд
+                finish();
+            }
+        });
 
+        //удаление
+        Button cancelGlass = findViewById(R.id.cancel_glass);
+        cancelGlass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo удаление стакана из бд
+                finish();
             }
         });
     }
