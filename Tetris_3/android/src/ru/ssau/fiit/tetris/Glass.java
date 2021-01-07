e2c2e3f5e5e804a38ce62e596f1810428270a1d6
daAnis <daria.anisimowa@yandex.ru>
2021-01-07 17:50:34 +0400

    commit

---
+++ Tetris_3/android/src/ru/ssau/fiit/tetris/Glass.java
@@ -0,0 +1,58 @@
+package ru.ssau.fiit.tetris;
+
+public class Glass {
+
+    private int width;
+    private int height;
+    private int color;
+    private double speed_k;
+    private double points_k;
+
+    public Glass(int width, int height, int color, double speed_k, double points_k) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.speed_k = speed_k;
+        this.points_k = points_k;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public double getSpeed_k() {
+        return speed_k;
+    }
+
+    public void setSpeed_k(double speed_k) {
+        this.speed_k = speed_k;
+    }
+
+    public double getPoints_k() {
+        return points_k;
+    }
+
+    public void setPoints_k(double points_k) {
+        this.points_k = points_k;
+    }
+}
--- Tetris_3/android/src/ru/ssau/fiit/tetris/GlassView.java
+++ Tetris_3/android/src/ru/ssau/fiit/tetris/GlassView.java
@@ -15,6 +15,8 @@ public class GlassView extends View {
     private Rect rect;
     private Paint paint;
 
+    private Glass glass;
+
     public GlassView(Context context) {
         super(context);
 
@@ -24,15 +26,29 @@ public class GlassView extends View {
     private void init(@Nullable AttributeSet set) {
         rect = new Rect();
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        glass = new Glass(1, 1,
+                getResources().getColor(R.color.check_box_color),
+                1.0, 1.0);
+    }
+
+    public void setGlass (Glass glass) {
+        this.glass.setWidth(glass.getWidth());
+        this.glass.setHeight(glass.getHeight());
+        this.glass.setColor(glass.getColor());
+        this.glass.setSpeed_k(glass.getSpeed_k());
+        this.glass.setPoints_k(glass.getPoints_k());
+
+        postInvalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        paint.setColor(getResources().getColor(R.color.check_box_color));
+        paint.setColor(glass.getColor());
         paint.setStyle(Paint.Style.STROKE);
-        rect.left = 50;
-        rect.top = 50;
+        
+        rect.left = 0;
+        rect.top = 0;
         rect.right = rect.left + CELL_SIZE;
         rect.bottom = rect.top + CELL_SIZE;
         canvas.drawRect(rect, paint);
